aa8b4b1a40770c52ff42676010e535a2ff4ffdcd
Koen van der Pool <koenvdpool@gmail.com>
2023-10-11 22:44:23 +0200

    Finish 4b part 1, with comments

--- Assignments/ted_english_analysis.py
+++ Assignments/ted_english_analysis.py
@@ -3,119 +3,172 @@ import utils
 
 def find_wc(talk_elements, /, length="longest"):
     """
+    Finds the talk(s) that is/are either the longest or shortest in word count.
 
+    :param talk_elements: list of talk elements
+    :param length: specifies the desired talk length, either "longest" or "shortest" (default is "longest")
+    :return: lists of title(s) and ID(s) for the selected talk(s), the word count of the selected talk(s),
+    and the average word count over all the talks
     """
+    # Create variables to store the selected word count (highest or lowest) and its corresponding talk(s)
     selected_word_count = 0
     selected_talks = []
+
+    # Create variable to store all word counts
     all_word_counts = []
 
+    # Loops over all the talks
     for talk in talk_elements:
         current_word_count = utils.get_word_count(talk)
+        # Check if talk has text, if not, continue to next talk
         if current_word_count <= 0:
             continue
 
+        # Append word count to the list of all word counts
         all_word_counts.append(current_word_count)
+
+        # Compare the word count of the current talk to the selected word count
         if length == "longest":
             if current_word_count > selected_word_count:
+                # If longer, adjust the selected word count and list of selected talks
                 selected_word_count = current_word_count
                 selected_talks = [talk]
             elif current_word_count == selected_word_count:
+                # If the word count is the same, append talk to the list of selected talks
                 selected_talks.append(talk)
 
         elif length == "shortest":
             if (selected_word_count == 0) or (current_word_count < selected_word_count):
+                # If zero or shorter, adjust the selected word count and list of selected talks
                 selected_word_count = current_word_count
                 selected_talks = [talk]
             elif current_word_count == selected_word_count:
+                # If the word count is the same, append talk to the list of selected talks
                 selected_talks.append(talk)
 
+    # Create lists to store the titles and IDs of the selected talks
     selected_titles = []
     selected_ids = []
+
+    # Get mean word count over all the talks
     mean_word_count = utils.get_mean_word_count(all_word_counts)
 
+    # Loops over the selected talk and add titles and IDs to the lists
     for talk in selected_talks:
         title = utils.get_title(talk)
         selected_titles.append(title)
         talk_id = utils.get_id(talk)
         selected_ids.append(talk_id)
 
+    # Return the titles, IDs, word count and mean word count
     return selected_titles, selected_ids, selected_word_count, mean_word_count
 
 
 def find_date(talk_elements, /, time="oldest"):
     """
+    Finds either the oldest or newest talk(s).
 
+    :param talk_elements: list of talk elements
+    :param time: specifies if you want the "oldest" of "newest" talk(s) (default is "oldest")
+    :return: the title(s) and ID(s) of the selected talk(s) as two lists
     """
 
-    selected_talks = []
+    # Create variables to store the selected date (oldest or newest) and its corresponding talk(s)
     selected_date = ''
+    selected_talks = []
 
+    # Loops over all the talks
     for talk in talk_elements:
-
         word_count = utils.get_word_count(talk)
+        # Check if talk has text, if not, continue to next talk
         if word_count <= 0:
             continue
 
+        # Get the date of the current talk
         current_date = utils.get_date(talk)
+
+        # Check for current talk if date exists
         if current_date != "No date available":
+            # If list of selected talks is empty, adjust the selected data and add current talk to the list
             if not selected_talks:
-                selected_talks = [talk]
                 selected_date = current_date
+                selected_talks = [talk]
             else:
+                # Compare the dates of the selected talk(s) and the current talk
                 dates_comparison = utils.compare_dates(selected_date, current_date)
                 if time == "newest":
                     if dates_comparison == 'newer':
-                        selected_talks = [talk]
+                        # If newer, adjust the selected date and list of selected talks
                         selected_date = current_date
+                        selected_talks = [talk]
                     elif dates_comparison == 'same':
+                        # If the date is the same, append talk to the list of selected talks
                         selected_talks.append(talk)
 
                 if time == "oldest":
                     if dates_comparison == 'older':
-                        selected_talks = [talk]
+                        # If older, adjust the selected date and list of selected talks
                         selected_date = current_date
+                        selected_talks = [talk]
                     elif dates_comparison == 'same':
+                        # If the date is the same, append talk to the list of selected talks
                         selected_talks.append(talk)
 
+    # Create lists to store the titles and IDs of the selected talks
     selected_titles = []
     selected_ids = []
 
+    # Loops over the selected talk and add titles and IDs to the lists
     for talk in selected_talks:
         title = utils.get_title(talk)
         selected_titles.append(title)
         talk_id = utils.get_id(talk)
         selected_ids.append(talk_id)
 
+    # Returns the title(s) and ID(s) of the selected talk(s)
     return selected_titles, selected_ids
 
 
 def find_speaker(talk_elements, /):
     """
+    Finds the speakers with more than one talk.
 
+    :param talk_elements: list of talk elements
+    :return: a dict of the speakers mapped to their talks
     """
+    # Create dict to map the speakers to their talks
     speaker_to_talks = {}
 
+    # Loops over the talks
     for talk in talk_elements:
         word_count = utils.get_word_count(talk)
+        # Check if talk has text, if not, continue to next talk
         if word_count <= 0:
             continue
 
+        # Get information of the current talk
         speaker = utils.get_speaker(talk)
         title = utils.get_title(talk)
         talk_id = utils.get_id(talk)
 
+        # If the speaker is not in the dict, add the speaker with an empty list to store the talks in
         if speaker not in speaker_to_talks:
             speaker_to_talks[speaker] = []
 
+        # Create a tuple with information of the current talk and add to the dict
         talk_info = (title, talk_id)
         speaker_to_talks[speaker].append(talk_info)
 
+    # Create dict for speaker with multiple (more than one) talks
     speaker_to_multiple_talks = {}
 
+    # Loop over the items of the speaker_to_talks dictionary
     for speaker, num_talks in speaker_to_talks.items():
-        if len(num_talks) > 1:
+        if len(num_talks) > 1:  # Check if speaker had multiple talks
+            # If the speaker had more than one talk, add to the dict
             speaker_to_multiple_talks[speaker] = num_talks
 
+    # Return dict of the speakers with multiple talks
     return speaker_to_multiple_talks
 
 
@@ -144,4 +197,3 @@ if __name__ == "__main__":
 
     find_speaker_dict = find_speaker(talks)
     print(f'The speakers with more than one talk are the following:\n{find_speaker_dict}')
-
--- Assignments/utils.py
+++ Assignments/utils.py
@@ -16,13 +16,20 @@ def get_talks(root, /):
 
 def get_title(talk, /):
     """
+    Finds the title of a given talk.
 
+    :param talk: the talk given as an XML element
+    :return: the title of the talk as a str
     """
+    # Define the path to the title element
     path_to_title = './head/title'
 
+    # Find the title element
     title_element = talk.find(path_to_title)
 
+    # Check if title element exists
     if title_element is not None:
+        # If element exists, extract its text
         title = title_element.text
     else:
         title = "No title available"
@@ -32,13 +39,20 @@ def get_title(talk, /):
 
 def get_id(talk, /):
     """
+    Finds the ID of a given talk.
 
+    :param talk: the talk given as an XML element
+    :return: the ID of the talk as a str
     """
+    # Define the path to the talk ID element
     path_to_id = './head/talkid'
 
+    # Find the talk ID element
     talk_id_element = talk.find(path_to_id)
 
+    # Check if talk ID element exists
     if talk_id_element is not None:
+        # If element exists, extract its text
         talk_id = talk_id_element.text
     else:
         talk_id = "No talk ID available"
@@ -48,13 +62,20 @@ def get_id(talk, /):
 
 def get_date(talk, /):
     """
+    Finds the date of a given talk.
 
+    :param talk: the talk given as an XML element
+    :return: the date of the talk as a str
     """
+    # Define the path to the date element
     path_to_date = './head/date'
 
+    # Find the date element
     date_element = talk.find(path_to_date)
 
+    # Check if data element exists
     if date_element is not None:
+        # If element exists, extract its text
         date = date_element.text
     else:
         date = "No date available"
@@ -64,26 +85,42 @@ def get_date(talk, /):
 
 def get_word_count(talk, /):
     """
+    Finds the word count of a given talk.
 
+    :param talk: the talk given as an XML element
+    :return: the word count of the talk as an int
     """
+    # Define the path to the wordnum element
     path_to_word_num = './head/wordnum'
 
+    # Find the wordnum element
     word_num_element = talk.find(path_to_word_num)
 
-    word_num = int(word_num_element.text)
+    # Check if wordnum element exists
+    if word_num_element is not None:
+        # If element exists, extract text of the wordnum element and cast to int
+        word_num = int(word_num_element.text)
+    else:
+        word_num = "No word count available"
 
     return word_num
 
 
 def get_mean_word_count(list_of_word_counts, /):
     """
+    Calculates the mean word count out of a list of word counts.
 
+    :param list_of_word_counts: all the word counts as a list of integers
+    :return: the mean word count
     """
+    # Initialize a count
     count = 0
 
+    # Add each word count in the list to the count
     for word_count in list_of_word_counts:
         count += word_count
 
+    # Divide the count by the length of the word count list to get the mean
     mean = count / len(list_of_word_counts)
 
     return mean
@@ -91,23 +128,38 @@ def get_mean_word_count(list_of_word_counts, /):
 
 def get_speaker(talk, /):
     """
+    Finds the speaker of a given talk.
 
+    :param talk: the talk given as an XML element
+    :return: the speaker of the talk as a str
     """
+    # Define the path to the speaker element
     path_to_speaker = './head/speaker'
 
+    # Find the speaker element
     speaker_element = talk.find(path_to_speaker)
 
-    speaker = speaker_element.text
+    # Check if speaker element exists
+    if speaker_element is not None:
+        # If element exists, extract text of the speaker element
+        speaker = speaker_element.text
+    else:
+        speaker = "Speaker text not available"
 
     return speaker
 
 
 def get_int_date(date, /):
     """
+    Converts a 'YYYY/MM/DD' date string to three integers representing the year, month, and day.
 
+    :param date: the date of a talk as a str in the format 'YYYY/MM-/DD'
+    :return: the year, the month, and the day of the date as integers
     """
+    # Split the three components of the date string into a list of strings
     split_date = date.split('/')
 
+    # Cast each element in the list to an integer
     year = int(split_date[0])
     month = int(split_date[1])
     day = int(split_date[2])
@@ -117,18 +169,26 @@ def get_int_date(date, /):
 
 def compare_dates(selected_date, current_date, /):
     """
+    Checks whether the current date is older than, newer than or the same as the selected date.
 
+    :param selected_date: the date of the selected talk as a str in the format 'YYYY/MM-/DD'
+    :param current_date: the date of the current talk as a str in the format 'YYYY/MM-/DD'
+    :return: a str denoting if the current date is older than, newer than or the same as the selected date
     """
+    # Convert the str dates to integer components
     selected_year, selected_month, selected_day = get_int_date(selected_date)
     current_year, current_month, current_day = get_int_date(current_date)
 
+    # Check if current date is newer than the selected date
     if current_year > selected_year or (current_year == selected_year
                                         and ((current_month > selected_month)
                                              or (current_month == selected_month
                                                  and current_day > selected_day))):
         result_comparison = "newer"
+    # Check if current date is the same as the selected date
     elif current_year == selected_year and current_month == selected_month and current_day == selected_day:
         result_comparison = "same"
+    # If neither of the previous two conditions are true, then the current date is older than the selected one
     else:
         result_comparison = "older"
 
